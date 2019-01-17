5a18a8214ec713e9f8003e5a1901b87ecb6df06d
Aitor <aitor.nogalesor@elorrieta-errekamari.com>
2019-01-17 19:36:57 +0100

    inicio conexion

--- Ethazi3/src/controlador/nagusia.java
+++ Ethazi3/src/controlador/nagusia.java
@@ -1,6 +1,9 @@
 package controlador;
 
+import modelo.konexioa;
+
 public class nagusia {
 //hola buenas no me funciona ahora el syso
 	// cambio
+	konexioa bd =new konexioa("ethazi");
 }
---
+++ Ethazi3/src/modelo/konexioa.java
@@ -0,0 +1,78 @@
+package modelo;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class konexioa {
+	private String  maquina;
+    private String  usuario;
+    private String  clave;
+    private int puerto          = 3306;
+    private String  servidor;
+    private static Connection conexion  = null;
+ 
+    //CONSTRUCTOR
+    //Recibe el nombre de la base de datos
+    public konexioa(String baseDatos){
+       
+    		String fichero = "src\\modelo\\Fitxeroa.txt";
+    		int kontagailua =0;
+    		String server="jdbc:mysql://";
+    		try {
+    		      FileReader fr = new FileReader(fichero);
+    		      BufferedReader br = new BufferedReader(fr);
+    		 
+    		      String linea;
+    		      while((linea = br.readLine()) != null) {
+    		    	  System.out.println(linea);
+    		        if(kontagailua==0) {
+    		        	this.maquina=linea;}
+    		      	if (kontagailua==1) {
+    		      		this.usuario=linea;}
+    		      	if (kontagailua==2) {
+    		      		this.clave=linea;}
+    		      	kontagailua++;
+    		      	linea="";
+    		      	}
+    		      
+    		      if (kontagailua==3) {
+    		    	  this.servidor="";
+    		    	  
+    		      }
+    		      
+    		 
+    		      fr.close();
+    		    }
+    		    catch(Exception e) {
+    		      System.out.println("Excepcion leyendo fichero "+ fichero + ": " + e);
+    		    }
+    	
+    	this.servidor="jdbc:mysql://"+this.maquina+":"+ this.puerto+"/"+baseDatos;
+    	
+        //Registrar el driver
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.err.println("ERROR AL REGISTRAR EL DRIVER");
+            System.exit(0); //parar la ejecución
+        }
+ 
+        //Establecer la conexión con el servidor
+        try {
+            conexion = DriverManager.getConnection("jdbc:mysql://"+this.maquina,this.usuario, this.clave);
+        } catch (SQLException e) {
+            System.err.println("ERROR AL CONECTAR CON EL SERVIDOR");
+            System.exit(0); //parar la ejecución
+        }
+        System.out.println("Conectado a "+baseDatos);
+    }
+ 
+
+    //Devuelve el objeto Connection que se usará en la clase Controller
+    public static Connection getConexion() {
+        return conexion;
+    }
+}
