f441ad1d9960be00fa67e3914e50857492ade4fd
Aitor <aitor.nogalesor@elorrieta-errekamari.com>
2019-01-24 14:18:43 +0100

    no peta cuando no hay na seleccionado

--- Ethazi3/src/vista/AukeratuLinea.java
+++ Ethazi3/src/vista/AukeratuLinea.java
@@ -1,26 +1,20 @@
 package vista;
 
-import javax.swing.JPanel;
 import java.awt.Color;
-import javax.swing.JTextField;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.util.Collections;
 
-import javax.swing.JList;
-import javax.swing.JScrollBar;
-import javax.swing.JLabel;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-
-import com.toedter.calendar.JDateChooser;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JTextPane;
 
 import controlador.Bitartekoa;
 import controlador.Linea;
-import javax.swing.JTextPane;
 
 public class AukeratuLinea extends JPanel {
 
@@ -28,15 +22,13 @@ public class AukeratuLinea extends JPanel {
 	 * Create the panel.
 	 */
 	public AukeratuLinea(JFrame window) {
-		
+
 		ArrayList<Linea> Lineak = new ArrayList<Linea>();
 
 		Lineak = Bitartekoa.artuLieneak();
 		this.setBounds(200, 200, 450, 400);
 		setBackground(Color.LIGHT_GRAY);
 		setLayout(null);
-
-		
 
 		// lista sortzen dugu
 		JList<String> list = new JList<String>();
@@ -58,9 +50,10 @@ public class AukeratuLinea extends JPanel {
 
 		ActionListener panelaJarraitu = new ActionListener() { // panela aldatzen duen actionListenerra
 			public void actionPerformed(ActionEvent arg0) {
-				String autLinea = "";
-				autLinea = list.getSelectedValue().toString();// listaren balioa hartzen dugu
-				if (autLinea.length() > 0) {
+				if (!list.isSelectionEmpty()) {// serbait autatu badugu
+
+					String autLinea = list.getSelectedValue().toString();// listaren balioa hartzen dugu
+
 					autLinea = autLinea.substring(0, 2);// listaren lehenengo bi balioak hartzen ditugu hau da kodea
 					AukeratuGeltokia panGeltoki = new AukeratuGeltokia(window, autLinea);
 					InterfaseNagusia.changeScene(window, panGeltoki);
@@ -77,18 +70,14 @@ public class AukeratuLinea extends JPanel {
 		JButton btnJarraitu = new JButton("JARRAITU");
 		btnJarraitu.setBounds(313, 304, 89, 23);
 		add(btnJarraitu);
-		
+
 		JTextPane txtpnAukeratuLinea = new JTextPane();
 		txtpnAukeratuLinea.setEditable(false);
 		txtpnAukeratuLinea.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		txtpnAukeratuLinea.setText("AUKERATU LINEA");
-		txtpnAukeratuLinea.setBounds(133, 11, 169, 31);
+		txtpnAukeratuLinea.setBounds(130, 10, 170, 30);
 		add(txtpnAukeratuLinea);
 		btnJarraitu.addActionListener(panelaJarraitu);
-
-		
-
-		
 
 	}
 }
