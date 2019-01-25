2c4d4122309c6142b0be6f5ad2c0c2bb280bfa2a
urtzi <urtzi.etxebarriaar@elorrieta_errekamari.com>
2019-01-25 08:26:59 +0100

    + botones fuera

--- Ethazi3/src/vista/Imprimakia.java
+++ Ethazi3/src/vista/Imprimakia.java
@@ -6,7 +6,10 @@ import javax.swing.JLabel;
 import java.awt.Font;
 
 public class Imprimakia extends JPanel {
-
+    private JLabel lblTxartela;
+    private JLabel lblInprimatzen;
+    private JLabel label;
+    private JLabel lblEskerrikAsko;
 	/**
 	 * Create the panel.
 	 */
@@ -14,22 +17,22 @@ public class Imprimakia extends JPanel {
 		setBackground(Color.LIGHT_GRAY);
 		setLayout(null);
 		
-		JLabel lblTxartela = new JLabel("TXARTELA");
+		lblTxartela = new JLabel("TXARTELA");
 		lblTxartela.setFont(new Font("DialogInput", Font.BOLD, 25));
 		lblTxartela.setBounds(157, 33, 127, 56);
 		add(lblTxartela);
 		
-		JLabel lblInprimatzen = new JLabel("INPRIMATZEN");
+		lblInprimatzen = new JLabel("INPRIMATZEN");
 		lblInprimatzen.setFont(new Font("DialogInput", Font.BOLD, 25));
 		lblInprimatzen.setBounds(136, 73, 174, 56);
 		add(lblInprimatzen);
 		
-		JLabel label = new JLabel(". . . . . . . . . . . . . . . . . ");
+		label = new JLabel(". . . . . . . . . . . . . . . . . ");
 		label.setFont(new Font("Tahoma", Font.PLAIN, 30));
 		label.setBounds(75, 128, 308, 49);
 		add(label);
 		
-		JLabel lblEskerrikAsko = new JLabel("ESKERRIK ASKO");
+		lblEskerrikAsko = new JLabel("ESKERRIK ASKO");
 		lblEskerrikAsko.setFont(new Font("DialogInput", Font.BOLD, 28));
 		lblEskerrikAsko.setBounds(109, 188, 241, 28);
 		add(lblEskerrikAsko);
